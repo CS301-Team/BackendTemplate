fe8ca54d10f1318cfe13ca4e162f1fa4c482d86a
takerens <itsamemario2002@gmail.com>
2025-09-01 18:16:18 +0800

    Initialization
    Java Springboot template created with dependencies
    
    Swagger/OpenAPI documentation added

---
+++ backend-template/src/main/java/com/cs301/backend/BackendTemplateApplication.java
@@ -0,0 +1,24 @@
+package com.cs301.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "CS301 Backend API",
+		version = "1.0",
+		description = "API documentation for CS301 Backend Template"
+	)
+)
+
+public class BackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTemplateApplication.class, args);
+	}
+
+}
